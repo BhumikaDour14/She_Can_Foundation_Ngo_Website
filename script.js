50fa4d1c472bc1b105ff9b061d19f4ef32c6d786
BhumikaDour14 <bhumikadour14@gmail.com>
2026-05-28 03:09:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function showMessage() {
+  alert('Thank you for showing interest in volunteering with She Can Foundation!');
+}
+const payNowBtn =
+document.getElementById("payNowBtn");
+
+payNowBtn.addEventListener("click", () => {
+
+   alert(
+   "💜 Thank You For Your Contribution!\n\nYour support helps empower women and create brighter futures."
+   );
+
+});
